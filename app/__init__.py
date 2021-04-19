05e7622ac37c88db260db4175424d4f0e0acfb68
Daniil <dadyarri@gmail.com>
2021-04-19 15:29:33 +0300

    feat(app): register blueprint of crud

--- app/__init__.py
+++ app/__init__.py
@@ -2,4 +2,10 @@ from flask import Flask
 
 
 def create_app():
-    return Flask(__name__)
+    from app import models
+    from crud import crud as crud_blueprint
+
+    app = Flask(__name__)
+    app.register_blueprint(crud_blueprint)
+
+    return app
