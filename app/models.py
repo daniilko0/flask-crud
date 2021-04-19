5c8abd9826e3a62b6470d73102ed880e70a3c4fc
Daniil <dadyarri@gmail.com>
2021-04-19 16:29:42 +0300

    feat(app): add constructor to Book

--- app/models.py
+++ app/models.py
@@ -2,13 +2,15 @@ import datetime
 
 
 class Book(object):
-    isbn: str
-    name: str
-    author: str
-    pages: int
-    year: int
-    added_on: datetime.datetime
-    deleted: bool
+
+    def __init__(self, isbn, name, author, pages, year, added_on, deleted):
+        self.isbn: str = isbn
+        self.name: str = name
+        self.author: str = author
+        self.pages: int = pages
+        self.year: int = year
+        self.added_on: datetime.datetime = added_on
+        self.deleted: bool = deleted
 
     def __repr__(self):
         return '<Book: {}>'.format(self.name)
